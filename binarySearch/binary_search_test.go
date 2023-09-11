d0e13b30c72d1932122525386b80e45b210991ea
cristianortiz <cristian.ortiznavia82@gmail.com>
2023-09-10 21:29:29 -0300

    change test array

--- binarySearch/binary_search_test.go
+++ binarySearch/binary_search_test.go
@@ -4,22 +4,22 @@ import "testing"
 
 func TestBinarySearch(t *testing.T) {
 
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	arr := []int{11, 21, 33, 41, 50, 69, 72, 89, 93, 1004, 1134, 1266, 13788}
 	t.Run("Find the middle element position", func(t *testing.T) {
-		result := binarySearch(arr, 7)
+		result := binarySearch(arr, 72)
 		if result != 6 {
-			t.Errorf("position 6 was expected but,  %d returned instead", result)
+			t.Errorf(" 6 was expected but,  %d returned instead", result)
 		}
 	})
 	t.Run("Find the first element position", func(t *testing.T) {
-		result := binarySearch(arr, 1)
+		result := binarySearch(arr, 11)
 		if result != 0 {
 			t.Errorf("position 0 was expected , but %d returned instead", result)
 		}
 	})
 
 	t.Run("Find the position of the last element", func(t *testing.T) {
-		result := binarySearch(arr, 13)
+		result := binarySearch(arr, 13788)
 		if result != 12 {
 			t.Errorf("Se esperaba 12, pero se obtuvo %d", result)
 		}
